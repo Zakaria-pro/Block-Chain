7f2bde10aa8092dbb4976b38dca82b16b20f6323
Hamedoun <zhamedoun1@gmail.com>
2021-12-12 22:48:39 +0100

    Update BlockChain.java

--- src/blockChain/BlockChain.java
+++ src/blockChain/BlockChain.java
@@ -7,18 +7,22 @@ public class BlockChain {
 	static ArrayList<Block> blockchain = new ArrayList<Block>();
 
 	public static void main(String[] args) {	
+		// Instatiate 3 Blocks
 		Block genesisBlock = new Block("Hi im the first block", "0") ;
 		Block secondBlock = new Block("Yo im the second block",genesisBlock.hash) ;
 		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash) ;
 		
+		// Create a chain
 		blockchain.add(genesisBlock);
 		blockchain.add(secondBlock);
 		blockchain.add(thirdBlock);
 		
+		// System.out.println(blockchain);
+
 		String blockchainJson = StringUtil.getJson(blockchain);
+		System.out.println(blockchainJson);
 		
 		System.out.println("the number of blocks in the blockchain : "+blockchain.size());
-		System.out.println(blockchainJson);
 		
 		
 	}
